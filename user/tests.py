bd35a798fd4a6f48d8a47629894b70e9b3b05753
jay <jay@hakdokman.com>
2020-09-23 16:59:35 +0900

    회원 관리 testcase

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+from user.models import User
+from django.urls import reverse
+import json 
 
-# Create your tests here.
+
+class UserListTest(TestCase):
+    url='/user/'
+    def setUp(self):
+        self.client = APIClient()
+        self.user_data={
+            "user_nm": "testuser",
+            "email": "test@testuser.com",
+            "phone": "01000000"}
+        self.response=self.client.post(self.url,self.user_data,format='json')
+   
+    def test_user_registration(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+        
+    def test_get_list_user(self):
+
+        response =self.client.get(self.url,format='json')
+        self.assertEqual(status.HTTP_200_OK,response.status_code)
+        self.assertTrue(len(json.loads(response.content))==User.objects.count())
+        
+
+class UserDetailTest(TestCase):
+     def setUp(self):
+        self.client = APIClient()
+        self.user_nm="testuser"
+        self.phone= "01000000"
+        self.email= "test@testuser.com"
+        
+        self.user=User.objects.create(user_nm=self.user_nm, phone=self.phone, email=self.email)
+        self.url=reverse("details", kwargs={"pk":self.user.id})
+      
+
+     def test_UserDetail_can_get_a_user(self):
+        user=User.objects.get()
+        response = self.client.get(
+                reverse('details', kwargs={'pk': user.id}),
+                    format='json')
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertContains(response,user.id)
+        
+
+     def test_UserDetail_updte(self):
+        response=self.client.patch(self.url,{"done":True})
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,5 @@ from .import views
 
 urlpatterns=[
     path("", views.UserList.as_view()),
-    path("<int:pk>",views.UserDatail.as_view()),
+    path("<int:pk>",views.UserDatail.as_view(),name="details"),
 ]
